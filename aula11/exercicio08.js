6ef1ca1d5f79da612d2a286981b849dc72e74600
Gabriel Castillo <castillogabriel.cc@gmail.com>
2025-06-01 12:17:07 -0400

    practice files from lecture number 11

---
+++ aula11/exercicio08.js
@@ -0,0 +1,8 @@
+var velocidade = 40;
+
+console.log(`A velocidade do carro é ${velocidade} KM/h`);
+if (velocidade > 60) {
+  // Condição simples
+  console.log(`O carro está a ${velocidade}, você foi multado!`);
+}
+console.log("Lembre-se de sempre dirigir com segurança!");
